171b038aa96439dc2cf2b85b9c443b12b0e5c898
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2026-02-01 14:56:01 +0530

    more views added

--- 00.gettingstarted/intro/introapp/urls.py
+++ 00.gettingstarted/intro/introapp/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('index/', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('shivansh/', views.shivansh, name='shivansh'),
 ]
--- 00.gettingstarted/intro/introapp/views.py
+++ 00.gettingstarted/intro/introapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Hello, welcome to the Intro App!")
+
+def about(request):
+    return HttpResponse("This is the About page of the Intro App.")
+
+def contact(request):
+    return HttpResponse("This is the Contact page of the Intro App.")
+
+def shivansh(request):
+    return HttpResponse("Hello Shivansh! Welcome to the Intro App.")
