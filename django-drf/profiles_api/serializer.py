fa99153adc303290ebf4c8ebaf62cd5b28ac61a9
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2026-05-19 04:06:06 +0530

    add:added profile feed viewset

--- django-drf/profiles_api/serializer.py
+++ django-drf/profiles_api/serializer.py
@@ -37,3 +37,29 @@ class UserProfileSerializers(serializers.ModelSerializer):
             instance.set_password(password)
         return super().update(instance, validated_data)
 
+class ProfileFeedItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=models.ProfileFeedItem
+        fields=('id','user_profile','status_text','created_on')
+        extra_kwargs={
+            'user_profile':{
+                'read_only':True
+            }
+        }
+
+#this serializer will be used to serialize the ProfileFeedItem model. 
+# it will be used to convert the ProfileFeedItem model into a json format that can be 
+# sent in the response of the api. 
+# it will also be used to convert the json format that is sent in the request of the api into a 
+# ProfileFeedItem model that can be saved in the database.
+
+#the interaction with ProfileFeedItem model will be handled by the viewset that we will create in the views.py file.
+#but will we use the serializer to save the data in the database ? 
+# yes, we will use the serializer to save the data in the database. 
+# we will use the serializer to validate the data that is sent in the request and then we will use the serializer 
+# to save the data in the database. 
+# we will do that in the viewset that we will create in the views.py file.
+
+#but we havent created a def create method in the serializer ?
+# we will not create a def create method in the serializer because 
+# we will use the default create method that is provided by the ModelSerializer class.
--- django-drf/profiles_api/views.py
+++ django-drf/profiles_api/views.py
@@ -21,3 +21,39 @@ class UserProfileViewset(viewsets.ModelViewSet):
 
 class UserLoginApiView(ObtainAuthToken):
     renderer_classes= api_settings.DEFAULT_RENDERER_CLASSES
+
+
+
+class ProfileFeedItemViewSet(viewsets.ModelViewSet):
+    authentication_classes=(TokenAuthentication,)
+    serializer_class=serializer.ProfileFeedItemSerializer
+    queryset=models.ProfileFeedItem.objects.all()
+    permission_classes=(permissions.UpdateOwnStatus,)
+
+#override the perform_create() method to set the user profile to the logged in user when creating a new feed item.
+#a default perform_create() method is provided by the ModelViewSet class, 
+# but we need to override it to set the user profile to the logged in 
+# user when creating a new feed item.
+#default perform_create() method is used to save the new object in the database.
+# and it saves the new object in the database by calling the save() method of the serializer.
+# does it save all the fields of the serializer in the database ?
+# it saves all the fields of the serializer in the database except the read-only fields.
+# so if we have any read-only fields in the serializer, then those fields will not be saved in the database when we call the save() method of the serializer.
+# so in our case, the user_profile field is a read-only field in the ProfileFeedItemSerializer,
+# so when we call the save() method of the serializer, it will not save the user_profile field in the database.
+#then why we created a custom perform_create() method ?
+# we created a custom perform_create() method to set the user_profile field to the logged in user when creating a new feed item.
+# but how will it save if its a read-only field ?
+# it will save because we are overriding the perform_create() method and we are setting the user_profile field to the logged in user before calling the save() method of the serializer.
+#so  is perform_create() a serializer method or a viewset method ?
+# perform_create() is a viewset method. it is a method that is provided by the ModelViewSet class and it is used to save the new object in the 
+# database when we call the create() method of the viewset. 
+# so when we call the create() method of the viewset, 
+# it will call the perform_create() method to save the new object in the database. 
+# and we are overriding the perform_create() method to set the 
+# user_profile field to the logged in user before saving the new object in the database.
+# but user_profile is a read-only field in the serializer, so how will it save in the database ?
+# it will save because we are setting the user_profile field to the logged in user before calling the save() method of the serializer.
+    def perform_create(self,serializer):
+        """Sets the user profile to the logged in user"""
+        serializer.save(user_profile=self.request.user)
